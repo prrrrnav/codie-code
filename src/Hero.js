c45a1517bac6d9f082fd134559cb5e5cd241960e
Pranav <Pranav143010@gmail.com>
2024-03-20 01:04:00 +0530

    added routing in website

--- src/Hero.js
+++ src/Hero.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='  pb-10 text-white'>
+        <div className='  md:px-10 pb-10 text-white'>
             {/* <hr className=' mb-10'></hr> */}
             <div className='px-10 text-4xl pt-10 text-center hover: origin-top transition-all  duration-1000 mb-32 '>Here we are all about <br></br> <i className='text-8xl text-gray-500 '>coding our way up</i> </div>
             <img src='https://images.unsplash.com/photo-1626624338641-b99e0d32c958?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='hero' className='mt-20 w-full h-90 object-cover rounded-xl shadow-lg 
             border
-            hover:opacity-80 transition duration-300 ease-in-out border-white md:px-10 '  />
+            hover:opacity-80 transition duration-300 ease-in-out border-white  '  />
             {/* <div class="relative h-90">
                 <img src="https://images.unsplash.com/photo-1589287707312-213624549c88?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Paparazzi" class=" w-full mt-20 h-auto rounded-xl object-contain "></img>
                     <div class="absolute inset-0 flex items-center
